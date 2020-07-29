ab532944535150f19b5fdc4cfdf68a8df3f4f0ab
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-07-29 16:08:51 +1000

    Add morphology classifications and indicator analysis.

--- Codes/GoM_community_analyses.R
+++ Codes/GoM_community_analyses.R
@@ -16,6 +16,16 @@ composition$group <- ifelse(composition$Year <= 2009, "2005-2009", NA)
 composition$group <- ifelse(composition$Year > 2009 & composition$Year <=2015, "2010-2015", composition$group)
 composition$group <- ifelse(composition$Year > 2015, "2016-2017", composition$group)
 
+# if doing by morphology
+# composition$Branching <- rowSums(composition[,c("ACB", "CB")])
+# composition$Foliose <- rowSums(composition[,c("ACF", "CF")])
+# composition$Encrusting <- rowSums(composition[,c("ACE", "CE")])
+# composition$Submassive <- composition$CS
+# composition$Massive <- composition$CM
+# composition$Digitate <- composition$ACD
+# composition$Table <- composition$ACT
+# coral_types <- c("Branching", "Foliose", "Encrusting", "Submassive", "Massive", "Digitate", "Table")
+
 # NMDS plots by island 
 coral_types <- c("ACB", "ACT", "ACD", "ACF", "ACE", "CM", "CS", "CB", "CF", "CE")
 islands <- unique(composition$Island)
@@ -45,3 +55,21 @@ for (j in islands){
   ano.tmp <- data.frame(j, round(ano$statistic, 4), ano$signif)
   write.table(ano.tmp, file = "Results/Anosim_results.csv", sep = ",", append = TRUE, quote = FALSE, col.names = FALSE, row.names = FALSE)
 }
+
+# Indicator species analysis by island
+# https://jkzorz.github.io/2019/07/02/Indicator-species-analysis.html
+inv.df <- data.frame(matrix(ncol = 8, nrow = 0))
+colnames(inv.df) <- c("s.2005-2009", "s.2010-2015", "s.2016-2017", "index", "stat", "p.value", "Coral_type", "Island")
+fileName <- paste0("Results/Indicator_spp_analyses/ISA_coral_types.csv")
+write.csv(inv.df, fileName, row.names = F)
+
+for (k in islands){
+  xdf <- subset(composition, Island == k)
+  com = xdf[,coral_types]
+  inv = multipatt(com, xdf$group, func = "r.g", control = how(nperm = 9999))
+  inv.df <- as.data.frame(inv$sign, colnames = TRUE)
+  inv.df$Coral_type <- row.names(inv.df)
+  inv.df$Island <- k
+  write.table(inv.df, file = fileName, sep = ",", append = TRUE, quote = FALSE, col.names = FALSE, row.names = FALSE)
+}
+

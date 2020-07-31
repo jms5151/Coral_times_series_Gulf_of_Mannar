5231239fdca69154ebdabeead4b260ae3c960842
jms5151 <jamie.sziklay@gmail.com>
2020-07-31 17:33:00 +1000

    change benthic cover covariate calculations.

--- Codes/format_response_vars.R
+++ Codes/format_response_vars.R
@@ -8,8 +8,7 @@ composition <- read.csv("Data/GoM_data_2005_to_2017_Checked.csv", head = T)
 # total coral cover
 coral_types <- c("ACB", "ACT", "ACD", "ACF", "ACE", "CM", "CS", "CB", "CF", "CE")
 composition$total_ccov <- rowSums(composition[,coral_types])
-composition$prop_acroporid_cov <- rowSums(composition[,coral_types[1:5]])/composition$total_ccov
-  
+
 # reorder 
 composition <- composition[order(composition$LIT_Number, composition$Site_Number, composition$Island, composition$Year),]
 
@@ -18,7 +17,7 @@ changeFun <- function(vec_name) {vec_name - lag(vec_name)}
 composition[, c("delta_ccov", "delta_algae")] <- lapply(composition[, c("total_ccov", "Algae")], changeFun)
 
 lagFun <- function(vec_name) {lag(vec_name)}
-benthic_types <- c("total_ccov", "prop_acroporid_cov", "Algae", "CCA")
+benthic_types <- c("total_ccov", "Algae")
 lag_benthic_types <- paste0("lagged_", benthic_types)
 composition[, lag_benthic_types] <- lapply(composition[, benthic_types], lagFun)
 
